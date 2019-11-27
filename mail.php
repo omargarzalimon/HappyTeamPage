e39fd1d96e966a65282c2489710e45295eb61e26
Irvin Reyes <ireyesd1995@gmail.com>
2019-11-27 08:41:27 -0600

    mail
    mail

--- mail.php
+++ mail.php
@@ -2,20 +2,26 @@
 $name  = $_POST['name'];
 $email = $_POST['email'];
 $text = $_POST['text'];
+$enviados = 0;
 
-$para  = 'oscar.villegas@ab-inbev.com' . ', '; 
-$para  = 'paula.alba@ab-inbev.com' . ', '; 
-$para  = 'wendy.herrera@ab-inbev.com' . ', '; 
-$para  = 'omar.garza@ab-inbev.com' . ', '; 
-$para  = 'daniel.hernandeza@ab-inbev.com' . ', '; 
-$para  = 'irvin.reyesd@ab-inbev.com' . ', '; 
-$para  = 'omargarzalimon@gmail.com' . ', '; 
-$para .= 'jose.gallegosm@ab-inbev.com';
-$titulo    = 'Mensaje para Happy Team de '. $name;
-$mensaje   = $text;
-$cabeceras = 'From: '. $email . "\r\n" .
-    'Reply-To: happyteam@happyteamabi.com' . "\r\n" .
+$titulo    = 'Mensaje para Happy Team de '. $name ;
+$mensaje   = 'correo ' . $email .':' .$text;
+$cabeceras = 'From: info@happyteamabi.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-
-mail($para, $titulo, $mensaje, $cabeceras);
+    
+try{
+mail('34_93@hotmail.com', $titulo, $mensaje, $cabeceras);
+mail('paalsan@outlook.es', $titulo, $mensaje, $cabeceras);
+mail('wendha86@gmail.com', $titulo, $mensaje, $cabeceras);
+mail('danyhell7@hotmail.com', $titulo, $mensaje, $cabeceras);
+mail('ireyesd1995@gmail.com', $titulo, $mensaje, $cabeceras);
+mail('omargarzalimon@gmail.com', $titulo, $mensaje, $cabeceras);
+mail('redgallegos02@gmail.com', $titulo, $mensaje, $cabeceras);
+$enviados = 1;
+} catch (Exception $e) {
+    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+}
+if($enviados == 1){
+header('Location: https://happyteamabi.com/home/contact.html');
+}
 ?>
